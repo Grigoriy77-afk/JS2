841e98fa1bb07b650e636a3308efc005f127be20
Grigoriy77 <vologzhannikov77@mail.ru>
2019-10-04 15:45:50 +0300

    Урок 3

--- JS2_lesson1/script.js
+++ JS2_lesson1/script.js
@@ -1,21 +1,37 @@
 'use strict';
 
+    function sendRequest (url) {
+        return new Promise((resolve, reject) => { //Создаем новый объект "promis" c помощью "new" и встроен. класса Promise
+            const xhr = new  XMLHttpRequest();            //встроенные функции, чтобы переводить promise из одного
+            xhr.open(`GET`, url);                         // состояния в другое (из pending => fulfiled || rejected)
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status !== 200) {
+                        reject();     //Если что-то пошло не так
+                    }
+                    const users = JSON.parse(xhr.responseText);
+
+                    resolve (users);  //Если всё верно
+                }
+            };
+            xhr.send();
+        });
+    }
+
     class ItemsList {    //Список товаров
         constructor () {
             this.items = [];
         }
 
         fetchItems () {
-            this.items = [
-                {id: 1, title: `Компьютерная мышь`, price: 400},
-                {id: 2, title: `Жесткий диск SSD 1Tb`, price: 10000},
-                {id: 3, title: `Материнская плата`, price: 4000},
-                {id: 4, title: `Видео карта`, price: 15000},
-            ];
+            return sendRequest(`/goods`).then((items) => {
+                this.items = items;  //Получаем с сервера
+            });
         }
 
+
         total() {
-            return  this.items.reduce((sum, current) => sum + current.price);
+            return  this.items.reduce((sum, item) => sum + item.price, 0);
          }
 
         render () {
@@ -39,9 +55,13 @@
     }
 
     const items = new ItemsList();
-    items.fetchItems();
+    items.fetchItems().then(() => {      //then, чтобы дождаться ответа сервера
+        document.querySelector(`.catalog`).innerHTML = items.render();
+    });
 
-    document.querySelector(`.catalog`).innerHTML = items.render();
+
+
+    document.querySelector(`.total`).innerHTML = `Общая стоимость товаров составляет: ${items.total()}`;
 
                              //Классы для корзины
     class CartList {
@@ -49,14 +69,19 @@
             this.goods = [];
         }
 
+        fetchGoods () {
+            return sendRequest(`/cart`).then((goods) => {
+                this.goods = goods;  //Получаем с сервера
+            });
+        }
 
-        fetchGoods() {
-            this.goods = [
-                {id: 1, title: `Компьютерная мышь`, price: 400},
-                {id: 2, title: `Жесткий диск SSD 1Tb`, price: 10000},
-                {id: 3, title: `Материнская плата`, price: 4000},
-                {id: 4, title: `Видео карта`, price: 15000},
-            ];
+
+        total() {
+            let total = 0;
+            for (let i = 0; i < this.goods.length; i++) {
+                total = total + this.goods [i].price * this.goods [i].quantity;
+            }
+            return total;
         }
 
         render() {
@@ -65,9 +90,10 @@
     }
 
     class Good {
-        constructor (title, price) {
+        constructor (title, price, quantity) {
             this.title = title;
             this.price = price;
+            this.quantity = quantity;
         }
         render () {
             return `<div class="good">
@@ -76,7 +102,16 @@
                         <p>${this.price}</p>
                         <button class="by-btn">Купиить</button></div>`
         }
+
+        sum(){
+            return this.price * this.quantity;
+        }
     }
+
+    const carts = new CartList();
+    items.fetchGoods().then(() => {      //then, чтобы дождаться ответа сервера
+        document.querySelector(`.cart`).innerHTML = items.render();
+    });
 
 
 
